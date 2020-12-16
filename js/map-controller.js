a2c628dd8abf140b24a59808d42d04638a80df97
Tamir Leshetz <tamir.leshetz@gmail.com>
2020-12-16 14:25:04 +0200

    added style

--- js/map-controller.js
+++ js/map-controller.js
@@ -1,6 +1,4 @@
-
-import {locationService} from './services/location-service.js'
-
+import { locationService } from './services/location-service.js';
 
 console.log('locationService', locationService);
 
@@ -14,39 +12,36 @@ window.onload = () => {
         .catch(console.log('INIT MAP ERROR'));
 
     getUserPosition()
-        .then(pos => {
+        .then((pos) => {
             console.log('User position is:', pos.coords);
         })
-        .catch(err => {
+        .catch((err) => {
             console.log('err!!!', err);
-        })
+        });
 
     document.querySelector('.btn').addEventListener('click', (ev) => {
         console.log('Aha!', ev.target);
         panTo(35.6895, 139.6917);
-    })
-}
-
+    });
+};
 
 export function initMap(lat = 32.0749831, lng = 34.9120554) {
     console.log('InitMap');
-    return _connectGoogleApi()
-        .then(() => {
-            console.log('google available');
-            gGoogleMap = new google.maps.Map(
-                document.querySelector('#map'), {
-                center: { lat, lng },
-                zoom: 15
-            })
-            console.log('Map!', gGoogleMap);
-        })
+    return _connectGoogleApi().then(() => {
+        console.log('google available');
+        gGoogleMap = new google.maps.Map(document.querySelector('#map'), {
+            center: { lat, lng },
+            zoom: 15,
+        });
+        console.log('Map!', gGoogleMap);
+    });
 }
 
 function addMarker(loc) {
     var marker = new google.maps.Marker({
         position: loc,
         map: gGoogleMap,
-        title: 'Hello World!'
+        title: 'Hello World!',
     });
     return marker;
 }
@@ -59,14 +54,13 @@ function panTo(lat, lng) {
 function getUserPosition() {
     console.log('Getting Pos');
     return new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject)
-    })
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
 }
 
-
 function _connectGoogleApi() {
-    if (window.google) return Promise.resolve()
-    const API_KEY = ''; //TODO: Enter your API Key
+    if (window.google) return Promise.resolve();
+    const API_KEY = 'AIzaSyA9krZ02aDNloGSkQmiwb-2XLuChoMHJh4'; //TODO: Enter your API Key
     var elGoogleApi = document.createElement('script');
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
     elGoogleApi.async = true;
@@ -74,9 +68,6 @@ function _connectGoogleApi() {
 
     return new Promise((resolve, reject) => {
         elGoogleApi.onload = resolve;
-        elGoogleApi.onerror = () => reject('Google script failed to load')
-    })
+        elGoogleApi.onerror = () => reject('Google script failed to load');
+    });
 }
-
-
-
